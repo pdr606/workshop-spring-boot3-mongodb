43f8ddb3876d1e46678f273ed1433f440f469815
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-08-24 21:53:36 -0300

    Consulta simples com query methods

--- src/main/java/com/example/demo/resoucers/PostResource.java
+++ src/main/java/com/example/demo/resoucers/PostResource.java
@@ -3,6 +3,7 @@ package com.example.demo.resoucers;
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.UserDTO;
+import com.example.demo.resoucers.util.URL;
 import com.example.demo.services.PostService;
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,14 @@ public class PostResource {
         Post obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 
 
